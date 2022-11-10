81b658f2b2953f15c86d902c9179e4f0d8a1d00c
michele <michele.selvaggi@cern.ch>
2022-11-10 15:01:35 +0100

    added CMS

--- macros/brem_electrons.py
+++ macros/brem_electrons.py
@@ -14,10 +14,10 @@ def rel_reso_total(e, det):
     e_brem = e * (1.0 - np.exp(-x_x0))
     e_tr = e - e_brem
 
-    sigma_tr = a * e_tr**2 + b * e_tr
-    sigma_calo = np.sqrt(n**2 + e_brem * s**2 + e_brem**2 * c**2)
+    sigma_tr = a * e_tr ** 2 + b * e_tr
+    sigma_calo = np.sqrt(n ** 2 + e_brem * s ** 2 + e_brem ** 2 * c ** 2)
 
-    return np.sqrt(sigma_tr**2 + sigma_calo**2) / e
+    return np.sqrt(sigma_tr ** 2 + sigma_calo ** 2) / e
 
 
 def rel_reso_trk(e, trk):
@@ -25,7 +25,7 @@ def rel_reso_trk(e, trk):
 
 
 def rel_reso_calo(e, calo):
-    return np.sqrt(calo.n**2 / e**2 + calo.s**2 / e + calo.c**2)
+    return np.sqrt(calo.n ** 2 / e ** 2 + calo.s ** 2 / e + calo.c ** 2)
 
 
 def plot_resolutions(resos, name):
@@ -73,14 +73,18 @@ class Det:
 
 idea_trk = Tracker(2.5e-05, 5.0e-04, 0.047)
 cld_trk = Tracker(1.0e-05, 2.5e-03, 0.115)
+cms_trk = Tracker(1.0e-04, 0.01, 0.50)
 
 idea_calo = Calo(0.05, 0.11, 0.01)
 cld_calo = Calo(0.05, 0.15, 0.01)
-cry_calo = Calo(0.05, 0.03, 0.005)
+cry_calo = Calo(0.12, 0.03, 0.003)
+cms_calo = Calo(0.12, 0.05, 0.01)
 
 idea = Det(idea_trk, idea_calo)
 idea_cry = Det(idea_trk, cry_calo)
 cld = Det(cld_trk, cld_calo)
+# cms = Det(cms_trk, cry_calo)
+cms = Det(cms_trk, cms_calo)
 
 e = np.arange(1.0, 200.1, 0.1)
 
@@ -95,6 +99,10 @@ reso_idea_cry_calo = rel_reso_calo(e, idea_cry.calo)
 reso_cld = rel_reso_total(e, cld)
 reso_cld_trk = rel_reso_trk(e, cld.tracker)
 reso_cld_calo = rel_reso_calo(e, cld.calo)
+
+reso_cms = rel_reso_total(e, cms)
+reso_cms_trk = rel_reso_trk(e, cms.tracker)
+reso_cms_calo = rel_reso_calo(e, cms.calo)
 
 
 resolutions_idea = {
@@ -115,14 +123,23 @@ resolutions_cld = {
     "CLD (calo only)": reso_cld_calo,
 }
 
+resolutions_cms = {
+    "CMS ": reso_cms,
+    "CMS (trk only)": reso_cms_trk,
+    "CMS (calo only)": reso_cms_calo,
+}
+
+
 resolutions_all = {
     "IDEA": reso_idea,
     "IDEA - crys": reso_idea_cry,
     "CLD ": reso_cld,
+    "CMS ": reso_cms,
 }
 
 
 plot_resolutions(resolutions_idea, "brem_idea")
 plot_resolutions(resolutions_idea_cry, "brem_idea_cry")
 plot_resolutions(resolutions_cld, "brem_cld")
+plot_resolutions(resolutions_cms, "brem_cms")
 plot_resolutions(resolutions_all, "brem_all")
